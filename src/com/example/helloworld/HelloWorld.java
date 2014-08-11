e6e6235da01135d3cebed6cf53a6f73d8e1fef3b
alisonps <alison.polton.simon@gmail.com>
2014-08-10 21:01:33 -0400

    initial commit

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by alisonps on 8/10/14.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
